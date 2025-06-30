b4597b1973d375511e5a7efd193db2452fcc41f7
gucciedits <132843368+gucciedits@users.noreply.github.com>
2025-06-30 12:29:11 -0300

    Updated index.js

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 const port = 3000;
 
-// Definindo uma rota simples
+// Definir o diretório de arquivos estáticos (CSS, JS, etc.)
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Rota para a página inicial
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.sendFile(path.join(__dirname, 'home.html'));
 });
 
-// Iniciando o servidor
+// Outras rotas para outras páginas
+app.get('/filmes', (req, res) => {
+  res.sendFile(path.join(__dirname, 'filmes.html'));
+});
+
+app.get('/perfil', (req, res) => {
+  res.sendFile(path.join(__dirname, 'perfil.html'));
+});
+
+// Iniciar o servidor
 app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+  console.log(`Servidor rodando em http://localhost:${port}`);
 });
